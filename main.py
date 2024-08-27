8c0753a5257b1cd4d48f1fddc1e4d0608d35f10f
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-28 00:35:25 +0500

    more functionality

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ grey = pygame.Color('grey12')
 white = (255, 255, 255)
 red = (255, 0, 0)
 blue = (0, 0, 255)
+green = (0, 255, 0)
 
 font = pygame.font.Font('freesansbold.ttf', 32)
 
@@ -60,23 +61,59 @@ while running:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            row = y // 200
-            col = x // 300
-            rect = pygame.Rect(col * 200, row * 200, 200, 200)
-            rects.append(rect)
-            try:
-                if board[row][col] == '-':
-                    if turn == 0:
-                        draw_x(row, col)
-                        turn = 1
-                    else:
-                        draw_o(row, col)
-                        turn = 0
-            except IndexError:
+            # print(x, y)
+            if x < 100 or x > 700:
                 pass
+            else: 
+                row = y // 200
+                col = x // 300
+                # rect = pygame.Rect((col * 200) + 100, row * 200, 200, 200)
+                # rects.append(rect)
+                try:
+                    if board[row][col] == '-':
+                        if turn == 0:
+                            draw_x(row, col)
+                            turn = 1
+                        else:
+                            draw_o(row, col)
+                            turn = 0
+                except IndexError:
+                    pass
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                board = [
+                    ['-', '-', '-'], 
+                    ['-', '-', '-'], 
+                    ['-', '-', '-']
+                    ]
+                turn = 0
+                rects = []
+                running = True
+        if event.type == pygame.MOUSEMOTION:
+            # highlight boxes
+            x, y = pygame.mouse.get_pos()
+            if x < 100 or x > 700:
+                pass
+            else:
+                row = y // 200
+                col = x // 300
+                rect = pygame.Rect((col * 200) + 100, row * 200, 200, 200)
+                rects.append(rect)
+                if len(rects) > 1:
+                    rects.pop(0)
 
     for rect in rects:
-        pygame.draw.rect(screen, red, rect, 5)
+        if turn == 0:
+            pygame.draw.rect(screen, blue, rect, 5)
+        else:
+            pygame.draw.rect(screen, green, rect, 5)
+    
+    for row in board:
+        # print(row)
+        if (row[0] == row[1] == row[2]) and row[0] != '-':
+            running = False
+            print('You win!')
+            break
 
     pygame.display.flip()
 
