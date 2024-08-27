4dd7cc584add087673b47f97ad0f09c161034766
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-27 22:28:15 +0500

    input and functionality

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ clock = pygame.time.Clock()
 # colors
 grey = pygame.Color('grey12')
 white = (255, 255, 255)
+red = (255, 0, 0)
+blue = (0, 0, 255)
+
+font = pygame.font.Font('freesansbold.ttf', 32)
 
 # variables
 turn = 0
@@ -20,9 +24,9 @@ turn = 0
 
 
 board = [
-    ['', '', ''], 
-    ['', '', ''], 
-    ['', '', '']
+    ['-', '-', '-'], 
+    ['-', '-', '-'], 
+    ['-', '-', '-']
     ]
 
 def draw_board():
@@ -30,27 +34,49 @@ def draw_board():
         for col in range(3):
             rect = pygame.FRect((col * 200) + 100, (row * 200) , 200, 200)
             pygame.draw.rect(screen, white, rect, 5)
+            text = font.render(board[row][col], True, white)
+            screen.blit(text, (rect.x + 100, rect.y + 100))
 
 def draw_x(row, col):
-    pass
+    board[row][col] = 'X'
 
 def draw_o(row, col):
-    pass
+    board[row][col] = 'O'
+
+
+rects = []
 
 # main loop
 running = True
 while running:
     clock.tick(60)
 
+    screen.fill(grey)
+    draw_board()
 
     # events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            x, y = pygame.mouse.get_pos()
+            row = y // 200
+            col = x // 300
+            rect = pygame.Rect(col * 200, row * 200, 200, 200)
+            rects.append(rect)
+            try:
+                if board[row][col] == '-':
+                    if turn == 0:
+                        draw_x(row, col)
+                        turn = 1
+                    else:
+                        draw_o(row, col)
+                        turn = 0
+            except IndexError:
+                pass
 
-
-    screen.fill(grey)
-    draw_board()
+    for rect in rects:
+        pygame.draw.rect(screen, red, rect, 5)
 
     pygame.display.flip()
 
