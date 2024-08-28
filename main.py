3ed07a048dc73fbf2b80bb8a82ab27c3a57aed09
abdurrafey-amir <abdurrafeyamir15@gmail.com>
2024-08-28 15:30:01 +0500

    functionality done, finished

--- main.py
+++ main.py
@@ -21,7 +21,10 @@ font = pygame.font.Font('freesansbold.ttf', 32)
 
 # variables
 turn = 0
-
+score_1 = 0
+# score1_text = font.render(f'Player 1: {score_1}', True, white)
+score_2 = 0
+# score_2_text = font.render(f'Player 2: {score_2}', True, white)
 
 
 board = [
@@ -44,6 +47,128 @@ def draw_x(row, col):
 def draw_o(row, col):
     board[row][col] = 'O'
 
+def reset_board():
+    global board, turn, rects, running
+
+    board = [
+    ['-', '-', '-'], 
+    ['-', '-', '-'], 
+    ['-', '-', '-']
+    ]
+    turn = 0
+    rects = []
+    running = True
+
+def restart():
+    global board, turn, score_1, score_2, rects, running
+    board = [
+    ['-', '-', '-'], 
+    ['-', '-', '-'], 
+    ['-', '-', '-']
+    ]
+    turn = 0
+    score_1 = 0
+    score_2 = 0
+    rects = []
+    running = True
+
+def check_win():
+    global running, score_1, score_2
+    
+    for row in board:
+        # print(row)
+        if (row[0] == row[1] == row[2]) and row[0] != '-':
+            # running = False
+            # print('You win!')
+            if row[0] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+            reset_board()
+            break
+        elif (board[0][0] == board[1][1] == board[2][2]) and board[0][0] != '-':
+            # running = False
+            # print('You win!')
+            if board[0][0] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+            reset_board()
+            break
+        elif (board[0][2] == board[1][1] == board[2][0]) and board[0][2] != '-':
+            # running = False
+            # print('You win!')
+            if board[0][2] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+            reset_board()
+            break
+        elif (board[0][0] == board[1][0] == board[2][0]) and board[0][0] != '-':
+            # running = False
+            # print('You win!')
+            if board[0][0] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+
+            reset_board()
+            break
+        elif (board[0][1] == board[1][1] == board[2][1]) and board[0][1] != '-':
+            # running = False
+            # print('You win!')
+            if board[0][1] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+
+            reset_board()
+            break
+        elif (board[0][2] == board[1][2] == board[2][2]) and board[0][2] != '-':
+            # running = False
+            # print('You win!')
+            if board[0][2] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+
+            reset_board()
+            break
+        elif (board[0][0] == board[0][1] == board[0][2]) and board[0][0] != '-':
+            # running = False
+            # print('You win!')
+            if board[0][0] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+
+            reset_board()
+            break
+        elif (board[1][0] == board[1][1] == board[1][2]) and board[1][0] != '-':
+            # running = False
+            # print('You win!')
+            if board[1][0] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+
+            reset_board()
+            break
+        elif (board[2][0] == board[2][1] == board[2][2]) and board[2][0] != '-':
+            # running = False
+            # print('You win!')
+            if board[2][0] == 'X':
+                score_1 += 1
+            else:
+                score_2 += 1
+
+            reset_board()
+            break
+        elif '-' not in board[0] and '-' not in board[1] and '-' not in board[2]:
+            # running = False
+            print('Draw!')
+            reset_board()
+            break
 
 rects = []
 
@@ -79,16 +204,11 @@ while running:
                             turn = 0
                 except IndexError:
                     pass
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_r:
-                board = [
-                    ['-', '-', '-'], 
-                    ['-', '-', '-'], 
-                    ['-', '-', '-']
-                    ]
-                turn = 0
-                rects = []
-                running = True
+                restart()
+
         if event.type == pygame.MOUSEMOTION:
             # highlight boxes
             x, y = pygame.mouse.get_pos()
@@ -108,12 +228,60 @@ while running:
         else:
             pygame.draw.rect(screen, green, rect, 5)
     
-    for row in board:
-        # print(row)
-        if (row[0] == row[1] == row[2]) and row[0] != '-':
-            running = False
-            print('You win!')
-            break
+
+    check_win()
+
+    score1_text = font.render(f'Player 1: {score_1}', True, white)
+    score_2_text = font.render(f'Player 2: {score_2}', True, white)
+    score1_text = pygame.transform.rotate(score1_text, 90)
+    score_2_text = pygame.transform.rotate(score_2_text, 270)
+    
+    sc = screen.blit(score1_text, (10, (HEIGHT // 2) - 90))
+    screen.blit(score_2_text, (WIDTH - 40, (HEIGHT // 2) - 100))
+
+    # for row in board:
+    #     # print(row)
+    #     if (row[0] == row[1] == row[2]) and row[0] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif (board[0][0] == board[1][1] == board[2][2]) and board[0][0] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif (board[0][2] == board[1][1] == board[2][0]) and board[0][2] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif (board[0][0] == board[1][0] == board[2][0]) and board[0][0] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif (board[0][1] == board[1][1] == board[2][1]) and board[0][1] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif (board[0][2] == board[1][2] == board[2][2]) and board[0][2] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif (board[0][0] == board[0][1] == board[0][2]) and board[0][0] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif (board[1][0] == board[1][1] == board[1][2]) and board[1][0] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif (board[2][0] == board[2][1] == board[2][2]) and board[2][0] != '-':
+    #         running = False
+    #         print('You win!')
+    #         break
+    #     elif '-' not in board[0] and '-' not in board[1] and '-' not in board[2]:
+    #         running = False
+    #         print('Draw!')
+    #         break
+
 
     pygame.display.flip()
 
